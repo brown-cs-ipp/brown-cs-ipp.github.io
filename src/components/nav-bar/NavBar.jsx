67bd6a8ec1cbbc2a8c89a7b36431922b7737e145
Siming <simingrox@gmail.com>
2023-10-24 16:52:51 -0400

    added links

--- src/components/nav-bar/NavBar.jsx
+++ src/components/nav-bar/NavBar.jsx
@@ -1,27 +1,32 @@
 import React from "react";
-import { useState } from 'react';
+import { useState } from "react";
 import "./NavBar.css";
 import { Link, NavLink } from "react-router-dom";
-import ipplogo from './images/ipp-logo.png'
-import brownlogo from './images/brownlogo.png'
-import { Divider, Typography } from '@mui/material';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
-import { MobileView, BrowserView, isBrowser, isMobile } from 'react-device-detect';
+import ipplogo from "./images/ipp-logo.png";
+import brownlogo from "./images/brownlogo.png";
+import { Divider, Typography } from "@mui/material";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
+import {
+  MobileView,
+  BrowserView,
+  isBrowser,
+  isMobile,
+} from "react-device-detect";
 
 // Create themes for typography and palette customization
 const theme = createTheme({
   typography: {
     fontFamily: "minion-pro",
-    fontWeightBold: 700
+    fontWeightBold: 700,
   },
   palette: {
     primary: {
-      main: '#193AA5',
+      main: "#193AA5",
     },
     secondary: {
-      main: '#FFFFFF',
+      main: "#FFFFFF",
     },
   },
 });
@@ -33,21 +38,21 @@ const theme2 = createTheme({
   },
   palette: {
     primary: {
-      main: '#193AA5',
+      main: "#193AA5",
     },
     secondary: {
-      main: '#FFFFFF',
+      main: "#FFFFFF",
     },
   },
 });
 
 export default function NavBar() {
   const [isNavExpanded, setIsNavExpanded] = useState(false);
-  const [url, setUrl] = useState('');
+  const [url, setUrl] = useState("");
 
   // Set the URL for Brown University logo link
   function handleClick() {
-    setUrl('https://cs.brown.edu/');
+    setUrl("https://cs.brown.edu/");
   }
 
   function toggleMobileMenu() {
@@ -59,139 +64,191 @@ export default function NavBar() {
       <div className="desktop-only">
         <div className="navbar-links-container">
           <ThemeProvider theme={theme2}>
-            <NavLink className="navbar-links"
-              to="/events" 
+            <NavLink
+              className="navbar-links"
+              to="/events"
               style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                background: isActive ? '#fff' : '#fff',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                })}
-              >
-                <Typography><b>EVENTS</b></Typography></NavLink>
-            <NavLink className="navbar-links"
+                color: isActive ? "#193AA5" : "#000000",
+                background: isActive ? "#fff" : "#fff",
+                borderBottom: isActive ? "2px solid #193AA5" : "none",
+              })}
+            >
+              <Typography>
+                <b>EVENTS</b>
+              </Typography>
+            </NavLink>
+
+            <NavLink
+              className="navbar-links"
               to="/partners"
               style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                background: isActive ? '#fff' : '#fff',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                })}
+                color: isActive ? "#193AA5" : "#000000",
+                background: isActive ? "#fff" : "#fff",
+                borderBottom: isActive ? "2px solid #193AA5" : "none",
+              })}
             >
-              <Typography><b>PARTNERS</b></Typography></NavLink>
-            <NavLink className="navbar-links"
+              <Typography>
+                <b>PARTNERS</b>
+              </Typography>
+            </NavLink>
+
+            <NavLink
+              className="navbar-links"
               to="/apply"
               style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                background: isActive ? '#fff' : '#fff',
-                })}
+                color: isActive ? "#193AA5" : "#000000",
+                borderBottom: isActive ? "2px solid #193AA5" : "none",
+                background: isActive ? "#fff" : "#fff",
+              })}
             >
-                <Typography><b>APPLY</b></Typography></NavLink>
-                <NavLink className="navbar-links"
+              <Typography>
+                <b>APPLY</b>
+              </Typography>
+            </NavLink>
+
+            <NavLink
+              className="navbar-links"
               to="/resources"
               style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                background: isActive ? '#fff' : '#fff',
-                })}
+                color: isActive ? "#193AA5" : "#000000",
+                borderBottom: isActive ? "2px solid #193AA5" : "none",
+                background: isActive ? "#fff" : "#fff",
+              })}
             >
-                <Typography><b>RESOURCES</b></Typography></NavLink>
+              <Typography>
+                <b>RESOURCES</b>
+              </Typography>
+            </NavLink>
           </ThemeProvider>
         </div>
       </div>
     );
   }
-  
+
   function buildMobileNavBarLinks() {
     return (
       <div className="mobile-menu">
         <div className="mobile-menu-open-button">
           <MenuIcon
-            style={{ color: '#193AA5', fontSize: '28px' }}
+            style={{ color: "#193AA5", fontSize: "28px" }}
             onClick={toggleMobileMenu}
           />
         </div>
 
         <ThemeProvider theme={theme2}>
-        <div className="navbar-links-row" id={isNavExpanded ? "menu-visible" : "menu-hidden"}>
-          <div className="navbar-links-mobile">
-            <NavLink to="/events"
-              onClick={toggleMobileMenu}
-              style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                background: isActive ? '#fff' : '#fff',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                })}>
-              <Typography><b>EVENTS</b></Typography>
-            </NavLink>
-          </div>
-          <div className="navbar-links-mobile">
-            <NavLink className="navbar-links-mobile"
-              onClick={toggleMobileMenu}
-              to="/partners"
-              style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                background: isActive ? '#fff' : '#fff',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                })}>
-              <Typography><b>PARTNERS</b></Typography>
-            </NavLink>
-          </div>
-          <div className="navbar-links-mobile">
-            <NavLink className="navbar-links-mobile"
-              onClick={toggleMobileMenu}
-              to="/apply"
-              style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                background: isActive ? '#fff' : '#fff',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                })}>
-              <Typography><b>APPLY</b></Typography>
-            </NavLink>
-          </div>
-          <div className="navbar-links-mobile">
-            <NavLink className="navbar-links-mobile"
-              onClick={toggleMobileMenu}
-              to="/resources"
-              style={({ isActive }) => ({
-                color: isActive ? '#193AA5' : '#000000',
-                background: isActive ? '#fff' : '#fff',
-                borderBottom: isActive ? '2px solid #193AA5' : 'none',
-                })}>
-              <Typography><b>RESOURCES</b></Typography>
-            </NavLink>
-          </div>
-  
+          <div
+            className="navbar-links-row"
+            id={isNavExpanded ? "menu-visible" : "menu-hidden"}
+          >
+            <div className="navbar-links-mobile">
+              <NavLink
+                to="/events"
+                onClick={toggleMobileMenu}
+                style={({ isActive }) => ({
+                  color: isActive ? "#193AA5" : "#000000",
+                  background: isActive ? "#fff" : "#fff",
+                  borderBottom: isActive ? "2px solid #193AA5" : "none",
+                })}
+              >
+                <Typography>
+                  <b>EVENTS</b>
+                </Typography>
+              </NavLink>
+            </div>
 
-          <div className="mobile-menu-close-button">
-            <CloseIcon
-              style={{ color: '#193AA5', fontSize: '28px' }}
-              onClick={toggleMobileMenu}
-            />
-          </div>
-        </div>
+            <div className="navbar-links-mobile">
+              <NavLink
+                className="navbar-links-mobile"
+                onClick={toggleMobileMenu}
+                to="/partners"
+                style={({ isActive }) => ({
+                  color: isActive ? "#193AA5" : "#000000",
+                  background: isActive ? "#fff" : "#fff",
+                  borderBottom: isActive ? "2px solid #193AA5" : "none",
+                })}
+              >
+                <Typography>
+                  <b>PARTNERS</b>
+                </Typography>
+              </NavLink>
+            </div>
 
+            <div className="navbar-links-mobile">
+              <NavLink
+                className="navbar-links-mobile"
+                onClick={toggleMobileMenu}
+                to="/apply"
+                style={({ isActive }) => ({
+                  color: isActive ? "#193AA5" : "#000000",
+                  background: isActive ? "#fff" : "#fff",
+                  borderBottom: isActive ? "2px solid #193AA5" : "none",
+                })}
+              >
+                <Typography>
+                  <b>APPLY</b>
+                </Typography>
+              </NavLink>
+            </div>
+
+            <div className="navbar-links-mobile">
+              <NavLink
+                className="navbar-links-mobile"
+                onClick={toggleMobileMenu}
+                to="/resources"
+                style={({ isActive }) => ({
+                  color: isActive ? "#193AA5" : "#000000",
+                  background: isActive ? "#fff" : "#fff",
+                  borderBottom: isActive ? "2px solid #193AA5" : "none",
+                })}
+              >
+                <Typography>
+                  <b>RESOURCES</b>
+                </Typography>
+              </NavLink>
+            </div>
+
+            <div className="mobile-menu-close-button">
+              <CloseIcon
+                style={{ color: "#193AA5", fontSize: "28px" }}
+                onClick={toggleMobileMenu}
+              />
+            </div>
+          </div>
         </ThemeProvider>
       </div>
     );
   }
 
   return (
-    <div className="navbar-container"> 
+    <div className="navbar-container">
       <div className="navbar-logo">
-        <a href={url}> <img className="brown" src={brownlogo} alt="brown university logo" onClick={handleClick}/> </a>
-        <Divider orientation="vertical" flexItem /> 
-        <Link to="/"> <img className="ipp" src={ipplogo} alt="ipp logo"/> </Link>
+        <a href={url}>
+          {" "}
+          <img
+            className="brown"
+            src={brownlogo}
+            alt="brown university logo"
+            onClick={handleClick}
+          />{" "}
+        </a>
+        <Divider orientation="vertical" flexItem />
+        <Link to="/">
+          {" "}
+          <img className="ipp" src={ipplogo} alt="ipp logo" />{" "}
+        </Link>
         <Link to="/">
           <ThemeProvider theme={theme}>
-            <BrowserView><Typography>Industry Partners Program</Typography></BrowserView>
-            <MobileView><Typography>IPP</Typography></MobileView>
+            <BrowserView>
+              <Typography>Industry Partners Program</Typography>
+            </BrowserView>
+            <MobileView>
+              <Typography>IPP</Typography>
+            </MobileView>
           </ThemeProvider>
         </Link>
       </div>
 
-      {isMobile
-      ? buildMobileNavBarLinks()
-      : buildDesktopNavBarLinks()}
+      {isMobile ? buildMobileNavBarLinks() : buildDesktopNavBarLinks()}
     </div>
   );
 }
--- src/views/resources/Resources.jsx
+++ src/views/resources/Resources.jsx
@@ -19,6 +19,10 @@ import { createTheme, ThemeProvider } from "@mui/material";
 import "./Resources.css";
 import brownSite from "./Brown-Site.jpg";
 import cabSite from "./CAB.png";
+import csDept from "./CS-Dept.png";
+import brownConnect from "./Brown-Connect.png";
+import handshake from "./Handshake.png";
+import climateTech from "./Climate-Tech.png";
 
 const theme = createTheme({
   typography: {
@@ -38,21 +42,45 @@ const theme = createTheme({
 const linksData = [
   {
     title: "Brown Site",
-    description: "Brown's Official Site",
+    description: "Brown's Official Site.",
     image: brownSite,
     link: "https://www.brown.edu/",
   },
   {
     title: "CAB",
-    description: "Courses @ Brown",
+    description: "Courses @ Brown.",
     image: cabSite,
     link: "https://cab.brown.edu/",
+  },
+  {
+    title: "Handshake",
+    description:
+      "Handshake is Brown’s online recruiting platform for students and alumni that simplifies the search for internships, jobs and learning opportunities.",
+    image: handshake,
+    link: "https://brown.joinhandshake.com/login",
+  },
+  {
+    title: "BrownConnect",
+    description:
+      "BrownConnect links students, alumni, parents and friends to extend learning beyond the classroom and promote mentoring and professional growth long past graduation.",
+    image: brownConnect,
+    link: "https://brownconnect.brown.edu/ds/index",
+  },
+  {
+    title: "CS Department Site",
+    description: "Official Site of the CS Department.",
+    image: csDept,
+    link: "https://cs.brown.edu/",
+  },
+  {
+    title: "ClimateTechList",
+    description: "Tracking hundreds of early career jobs, university internships, & fellowships from 1,003 climate tech companies.",
+    image: climateTech,
+    link: "https://www.climatetechlist.com/university",
   },
 ];
 
 export default function Resources() {
-  const [view, setView] = useState("grid");
-
   return (
     <BrowserView>
       <ThemeProvider theme={theme}>
@@ -60,77 +88,48 @@ export default function Resources() {
         <Typography variant="h4" align="center">
           Useful Resources
         </Typography>
-        <br />
-        <div
-          style={{
-            width: "80%",
-            margin: "auto",
-            display: "flex",
-            justifyContent: "center",
-          }}
-        >
-          {view === "grid" ? (
-            <>
-              <Button
-                variant="contained"
-                style={{ marginRight: "0.5rem" }}
-                onClick={() => setView("grid")}
-              >
-                Grid View
-              </Button>
-              <Button variant="outlined" onClick={() => setView("list")}>
-                List View
-              </Button>
-            </>
-          ) : (
-            <>
-              <Button
-                variant="outlined"
-                style={{ marginRight: "0.5rem" }}
-                onClick={() => setView("grid")}
-              >
-                Grid View
-              </Button>
-              <Button variant="contained" onClick={() => setView("list")}>
-                List View
-              </Button>
-            </>
-          )}
-        </div>
-        <br />
         <div style={{ width: "80%", margin: "auto" }}>
-          <Paper elevation={3} style={{ padding: "1rem" }}>
-            <Box m={4}>
-              <Grid container spacing={4}>
-                {linksData.map((item) => (
-                  <Grid item xs={view === "grid" ? 4 : 12} key={item.title}>
-                    <Card>
-                      <CardMedia
-                        component="img"
-                        height="200vh"
-                        image={item.image}
-                        alt={item.title}
-                      />
-                      <CardContent>
-                        <Typography variant="h5">{item.title}</Typography>
-                        <Typography>{item.description}</Typography>
-                        <br />
-                        <Button
-                          style={{ marginRight: "0.5rem" }}
-                          variant="outlined"
-                        >
-                          More Info
-                        </Button>
-                        <Button variant="contained" target="_blank" href={item.link}>
-                          Website
-                        </Button>
-                      </CardContent>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Box>
-          </Paper>
+          <Box m={4}>
+            <Grid container spacing={4} alignItems="stretch">
+              {linksData.map((item) => (
+                <Grid item xs={4} key={item.title}>
+                  <Card
+                    style={{
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
+                    }}
+                  >
+                    <CardMedia
+                      component="img"
+                      height="200vh"
+                      image={item.image}
+                      alt={item.title}
+                    />
+                    <CardContent style={{ flexGrow: 1 }}>
+                      <Typography variant="h5">{item.title}</Typography>
+                      <Typography>{item.description}</Typography>
+                    </CardContent>
+                    <Box p={2}>
+                      <Button
+                        style={{ marginRight: "0.5rem" }}
+                        variant="outlined"
+                      >
+                        More Info
+                      </Button>
+                      <Button
+                        variant="contained"
+                        target="_blank"
+                        href={item.link}
+                      >
+                        Website
+                      </Button>
+                    </Box>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Box>
         </div>
       </ThemeProvider>
     </BrowserView>
